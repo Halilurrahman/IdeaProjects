2ae70e2645ac72711f0e4664cacf55c786eca6ff
Halil Bagci <halilbagci75@hotmail.com>
2020-12-03 16:31:47 +0100

    java Interface assignments

--- TaxCalculator/src/com/halil/MoshTaxCalculator/TaxReport.java
+++ TaxCalculator/src/com/halil/MoshTaxCalculator/TaxReport.java
@@ -1,2 +1,14 @@
-package com.halil.MoshTaxCalculator;public class TaxReport {
+package com.halil.MoshTaxCalculator;
+
+public class TaxReport {
+    private TaxCalculator calculator;
+
+    public TaxReport() {
+calculator = new TaxCalculator( 100_000);
+
+    }
+    public void show() {
+        var tax = calculator.calculateTax();
+        System.out.println(tax);
+    }
 }
